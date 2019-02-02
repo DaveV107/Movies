6bace0d711107388dfde7c10b32869c140fbecbe
KillMurphy <leemurphy.work@gmail.com>
2019-02-02 16:43:04 +0000

    updated to include crud as per requirements doc

--- movies/src/test/java/com/company/catalogs/movies/controller/RatingsControllerTest.java
+++ movies/src/test/java/com/company/catalogs/movies/controller/RatingsControllerTest.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
@@ -17,6 +19,9 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
 
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
@@ -65,4 +70,53 @@ public class RatingsControllerTest {
         assertTrue(rating.getRating()==1);
     }
 
-}
+    @Test
+    public void addUpdateDeleteStatusTests() throws URISyntaxException {
+        RatingEntity rating = this.add(new RatingEntity(Long.valueOf(999), "TEST", "TV", "test rating"));
+        assertEquals("TEST", rating.getRatingCode());
+        rating.setRatingCode("TEST1");
+        rating = this.update(rating);
+        assertEquals("TEST1", rating.getRatingCode());
+        this.delete(rating.getRating());
+    }
+
+    public RatingEntity update(RatingEntity rating) {
+        HttpEntity<RatingEntity> ratingHttpEntity = new HttpEntity<>(rating);
+        HttpEntity<RatingEntity> ratingResponse = this.testRestTemplate.exchange(
+                baseUrl + ":" + this.port + "/api/v1/ratings",
+                HttpMethod.PUT,
+                ratingHttpEntity,
+                RatingEntity.class,
+                new HashMap<>());
+        RatingEntity returnRating = ratingResponse.getBody();
+        return returnRating;
+    }
+
+    public RatingEntity add(RatingEntity rating) {
+        HttpEntity<RatingEntity> ratingHttpEntity = new HttpEntity<>(rating);
+        HttpEntity<RatingEntity> ratingResponse = this.testRestTemplate.exchange(
+                baseUrl + ":" + this.port + "/api/v1/ratings",
+                HttpMethod.POST,
+                ratingHttpEntity,
+                RatingEntity.class,
+                new HashMap<>());
+        RatingEntity returnRating = ratingResponse.getBody();
+
+        assertEquals(HttpStatus.CREATED, ((ResponseEntity<RatingEntity>) ratingResponse).getStatusCode());
+
+        ResponseEntity<RatingEntity> ratingEntityResponse = this.testRestTemplate.getForEntity(
+                baseUrl + ":" + this.port + "/api/v1/ratings/1", RatingEntity.class);
+
+        assertEquals(HttpStatus.OK, ratingEntityResponse.getStatusCode());
+
+        return returnRating;
+
+    }
+
+    public void delete(Long rating) throws URISyntaxException {
+        URI uri = new URI(baseUrl + ":" + this.port + "/api/v1/ratings/" + rating);
+        this.testRestTemplate.delete(uri);
+        ResponseEntity<RatingEntity> ratingEntityResponse = this.testRestTemplate.getForEntity(
+                uri, RatingEntity.class);
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, ratingEntityResponse.getStatusCode());
+    }}
