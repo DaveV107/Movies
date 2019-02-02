61b744b6df1f43ccb68cc4cf63648e5b8399d63e
KillMurphy <leemurphy.work@gmail.com>
2019-02-02 16:41:30 +0000

    updated to include crud as per requirements doc

--- movies/src/main/java/com/company/catalogs/movies/service/impl/RatingServiceImpl.java
+++ movies/src/main/java/com/company/catalogs/movies/service/impl/RatingServiceImpl.java
@@ -4,8 +4,13 @@ import com.company.catalogs.movies.entity.RatingEntity;
 import com.company.catalogs.movies.repository.RatingRepository;
 import com.company.catalogs.movies.service.RatingService;
 import lombok.AllArgsConstructor;
+import org.springframework.hateoas.VndErrors;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 @Service
@@ -24,5 +29,37 @@ public class RatingServiceImpl implements RatingService {
     public RatingEntity one(Long rating) {
         return repository.findById(rating)
                 .orElseThrow(() -> new RuntimeException("cannot find rating with id " + rating));
+    }
+
+    @Override
+    public ResponseEntity<?> update(RatingEntity ratingEntity) throws URISyntaxException {
+        repository.findById(ratingEntity.getRating())
+                .orElseThrow(()->new RuntimeException("cannot find rating with id " + ratingEntity.getRating()));
+        RatingEntity rating = repository.save(ratingEntity);
+        return ResponseEntity
+                .created(new URI(rating.getRating().toString()))
+                .body(rating);
+    }
+
+    @Override
+    public ResponseEntity<?> add(RatingEntity ratingEntity) throws URISyntaxException{
+        RatingEntity rating = repository.save(ratingEntity);
+        return ResponseEntity
+                .created(new URI(rating.getRating().toString()))
+                .body(rating);
+    }
+
+    @Override
+    public ResponseEntity<?> delete(Long rating) {
+        if(repository.existsById(rating)) {
+            repository.deleteById(rating);
+            return ResponseEntity
+                    .noContent()
+                    .build();
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.GONE)
+                    .body(new VndErrors.VndError("not allowed", "does not exist"));
+        }
     }
 }
