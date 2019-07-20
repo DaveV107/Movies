27ea597aacc1a1d5eb62c9e22f0bf4866dbd38a5
killmurphy <6236821+KillMurphy@users.noreply.github.com>
2019-07-20 12:06:52 +0100

    created

---
+++ taskdependency/src/main/java/com/spuzzles/taskdependency/TaskdependencyApplication.java
@@ -0,0 +1,13 @@
+package com.spuzzles.taskdependency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskdependencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskdependencyApplication.class, args);
+    }
+
+}
